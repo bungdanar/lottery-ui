998f40e5e93219bb2b684c516ca2f4ff1eb61275
Bung Danar <danar.g.putera@gmail.com>
2023-03-19 09:52:29 +0700

    Finish UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import web3 from './web3'
+import lottery from './lottery'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [manager, setManager] = useState('')
+  const [players, setPlayers] = useState([])
+  const [balance, setBalance] = useState('')
+  const [value, setValue] = useState('')
+  const [message, setMessage] = useState('')
+
+  const fetchData = async () => {
+    const manager = await lottery.methods.manager().call()
+    const players = await lottery.methods.getPlayers().call()
+    const balance = await web3.eth.getBalance(lottery.options.address)
+
+    setManager(manager)
+    setPlayers(players)
+    setBalance(balance)
+  }
+
+  const handleEnter = async (event) => {
+    event.preventDefault()
+
+    setMessage('Waiting on transaction success...')
+
+    const accounts = await web3.eth.getAccounts()
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(value, 'ether'),
+    })
+
+    setMessage('You have been entered')
+  }
+
+  const handlePickWinner = async () => {
+    setMessage('Waiting on transaction success...')
+
+    const accounts = await web3.eth.getAccounts()
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+    })
+
+    setMessage('A winner has been picked!')
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   return (
-    <div className="App">
+    <div>
+      <h2>Lottery Contract</h2>
+      <div>This contract is managed by {manager}. </div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        There are currently {players.length} people entered, competing to win{' '}
+        {web3.utils.fromWei(balance)} ether!
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <hr />
+      <form onSubmit={handleEnter}>
+        <h4>Want to try your luck?</h4>
+        <div>
+          <label>Amount of ether to enter</label>
+          <input
+            type='text'
+            value={value}
+            onChange={(event) => setValue(event.target.value)}
+          />
+        </div>
+        <button type='submit'>Enter</button>
+      </form>
+      <hr />
+      <h4>Ready to pick a winner?</h4>
+      <button onClick={handlePickWinner}>Pick a winner!</button>
+      <hr />
+      <h1>{message}</h1>
     </div>
   )
 }
